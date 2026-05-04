79972a0f6da52c4432dfd00a9d601626023e68db
riadlam <riadlaamari@gmail.com>
2026-05-04 20:04:47 +0100

    ef

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/privacy_policy', function () {
+    return response()->file(public_path('privacy_policy.html'));
+});

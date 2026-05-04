b6f9500ae54392f05f970c63c493b49a7fa4458d
riadlam <riadlaamari@gmail.com>
2026-05-04 20:18:16 +0100

    d

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('/privacy_policy', function () {
     return response()->file(public_path('privacy_policy.html'));
 });
+
+Route::get('/terms_and_conditions', function () {
+    return response()->file(public_path('terms_and_conditions.html'));
+});
+
+Route::get('/terms', function () {
+    return response()->file(public_path('terms_and_conditions.html'));
+});
